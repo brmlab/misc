5612275fc807307b0cfe253f9f24b0c8218e67cc
Petr Baudis <pasky@ucw.cz>
2012-07-18 00:06:33 +0200

    barcodes.py -b: Wrap labels in 'brm lab' only if this switch is passed

--- barcodes.py
+++ barcodes.py
@@ -5,11 +5,19 @@
 #
 # example: seq -f %06g 0 1000 | ./barcodes.py
 #
+# options:
+# -b: label has format "brm - NUMER - lab" (for inventory stickers)
+#
 # requires zint binary from zint package
 #
 
 from subprocess import Popen, PIPE
+import argparse
 import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--brmlabel', action='store_true')
+args = parser.parse_args()
 
 svghead = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="1052.3622" height="744.09448" version="1.1" id="svg2" inkscape:version="0.47 r22583" sodipodi:docname="barcodes.svg">
@@ -34,7 +42,8 @@ try:
 	    elem = Popen(('zint','--directsvg','-d', data), stdout = PIPE).communicate()[0].split('\n')
 	    elem = elem[8:-2]
 	    elem[0] = elem[0].replace('id="barcode"', 'transform="matrix(%f,0,0,%f,%f,%f)"' % (scalex, scaley, 42+i*150 , 14+j*74.3) )
-	    elem[23] = 'brm - ' + elem[23].strip() + ' - lab'
+	    if args.brmlabel:
+		elem[23] = 'brm - ' + elem[23].strip() + ' - lab'
 	    f.write('\n'.join(elem))
 except EOFError:
     pass
